9971c3c6a2a908fb20c996337687db414ceda358
pkinney <pkinney@gmail.com>
2008-01-24 21:09:28 +0000

    Removed old migrations
    git-svn-id: http://clinicdb.rubyforge.org/svn@11 d096a246-4d2d-4550-b7bf-e545da479d79

--- db/migrate/014_removed_soap_fields.rb
+++
@@ -1,9 +0,0 @@
-class RemovedSoapFields < ActiveRecord::Migration
-  def self.up
-    remove_column :visits, :assessment
-  end
-
-  def self.down
-    add_column :visits, :assessment, :text
-  end
-end
