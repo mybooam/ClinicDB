80804b4d3e793996582119e8f1273e263e18a0fb
pkinney <pkinney@gmail.com>
2008-03-31 01:25:11 +0000

    Adding drugs on the fly now works
    git-svn-id: http://clinicdb.rubyforge.org/svn@39 d096a246-4d2d-4550-b7bf-e545da479d79

--- app/controllers/drug_controller.rb
+++ app/controllers/drug_controller.rb
@@ -10,26 +10,49 @@ class DrugController < ApplicationController
 	  redirect_to :action => 'test_drug'
   end
   
+  def new_drug
+    @drug_name = ""
+    @dose_unit = ""
+    if(params[:drug_name] && params[:drug_name].size>0)
+      d = Drug.from_user_string(params[:drug_name])
+      @drug_name = d.drug_name
+      @dose_unit = d.dose_unit
+    end
+  end
+  
+  def add_drug
+    s = "#{params[:drug][:name]}|#{params[:drug][:dosage]}"
+    d = Drug.from_user_string(s)
+    if d.save
+      flash[:notice] = "New drug added: #{d.to_label}"
+      redirect_to :controller => 'prescription', :action => 'new_for_patient', :drug_id => d.id, :patient_id => params[:patient_id]
+    else
+      flash[:error] = "Could not add drug #{d.to_label}"
+      redirect_to :controller => 'drug', :action => 'new_drug', :drug_name => s, :patient_id => params[:patient_id]
+    end
+  end
+  
   def live_search
-	    max_listing = 15
-      post_sections = request.raw_post.split('&')
-      if(post_sections.length>1)
-        phrases = post_sections[0].split('%20')
+    @patient = Patient.find(request.parameters[:patient_id])
+    post_sections = request.parameters.delete_if{ |k,v| k=="patient_id" || k=="authenticity_token" || k=="action" || k=="controller" }
+    @search_string = post_sections.keys.join(' ')
+    if(@search_string.size>0)
+      phrases = @search_string.split(' ')
+    
+      query_any = phrases.collect { |a|
+        "(name LIKE '%#{a}%')"
+      }.join(" OR ")
       
-        query_any = phrases.collect { |a|
-          "(name LIKE '%#{a}%')"
-        }.join(" OR ")
-        
-        query_all = phrases.collect { |a|
-          "(name LIKE '%#{a}%')"
-        }.join(" AND ")
+      query_all = phrases.collect { |a|
+        "(name LIKE '%#{a}%')"
+      }.join(" AND ")
+    
+      @results_all = Drug.find(:all, :conditions => query_all).sort{|a, b| a.name <=> b.name}
+      @results_any = Drug.find(:all, :conditions => query_any).sort{|a, b| a.name <=> b.name}
       
-        @results_all = Drug.find(:all, :conditions => query_all).sort{|a, b| a.name <=> b.name}
-        @results_any = Drug.find(:all, :conditions => query_any).sort{|a, b| a.name <=> b.name}
-        
-        @results = (@results_all + @results_any).uniq
-      end  
-
-      render(:layout => false)
+      @results = (@results_all + @results_any).uniq
+    end
+    
+    render(:layout => false)
   end
 end
--- app/controllers/prescription_controller.rb
+++ app/controllers/prescription_controller.rb
@@ -1,15 +1,23 @@
 class PrescriptionController < ApplicationController
 	active_scaffold :prescription do |config|
-		config.columns =[:given_date, :patient, :drug, :quantity, :units, :orders, :notes, :signed]
+		config.columns =[:given_date, :patient, :drug, :orders, :notes, :signed]
 		config.columns[:patient].form_ui = :select
 		config.columns[:drug].form_ui = :select
 	end
 
-    def add_for_patient
-      test = Prescription.new(params[:prescription])
-      test.given_date = Date.today()
-      test.save
+  def add_for_patient
+    test = Prescription.new(params[:prescription])
+    test.given_date = Date.today()
+    test.save
 
-      redirect_to :controller => 'home', :action => 'patient_home', :patient_id => test.patient_id
+    redirect_to :controller => 'home', :action => 'patient_home', :patient_id => test.patient_id
+  end
+  
+  def set_drug
+    if params[:drug_id]
+      redirect_to :action => 'new_for_patient', :drug_id => params[:drug_id], :patient_id => params[:patient_id]
+    else
+      redirect_to :controller => 'drug', :action => 'new_drug', :drug_name => params[:drug_name], :patient_id => params[:patient_id]
     end
+  end
 end
