623bf9acf99d6d28080e5dd2dafdb5a8f69f6a01
pkinney <pkinney@gmail.com>
2010-02-01 00:29:55 +0000

    Fixed bugged where users that had lowercase names would not sort right
    git-svn-id: http://clinicdb.rubyforge.org/svn@124 d096a246-4d2d-4550-b7bf-e545da479d79

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@
 module ApplicationHelper
   def user_field(name, options = {:size => 16}, selected = nil)
     # select object_name, method, User.find(:all, :conditions => { :active => true}).sort{|a,b| a.to_label <=> b.to_label}.collect{ |a| [a.to_label, a.id] }, {:size => options[:size], :include_blank => true}, :selected => (selected.nil? ? nil : selected.id)
-    options = User.find(:all, :conditions => { :active => true}).sort{|a,b| a.to_label <=> b.to_label}.collect{|a| "<option value='#{a.id}' #{(!selected.nil? && selected==a) ? 'selected=true' : ''}>#{a.to_label}</option>"}.join("")
+    options = User.find(:all, :conditions => { :active => true}).sort{|a,b| a.to_label.downcase <=> b.to_label.downcase}.collect{|a| "<option value='#{a.id}' #{(!selected.nil? && selected==a) ? 'selected=true' : ''}>#{a.to_label}</option>"}.join("")
     select_tag name, "<option value='' #{selected.nil? ? 'selected=true' : ''}></option>" + options
   end
   
