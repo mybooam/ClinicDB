f5a1d2c20420418e77c4d2d1fa0ad1abc6fc7d31
pkinney <pkinney@gmail.com>
2010-03-01 15:16:56 +0000

    Small change to setting_controller's activescaffold settings
    git-svn-id: http://clinicdb.rubyforge.org/svn@163 d096a246-4d2d-4550-b7bf-e545da479d79

--- app/controllers/setting_controller.rb
+++ app/controllers/setting_controller.rb
@@ -5,6 +5,10 @@ class SettingController < ApplicationController
       list.sorting = [{:key => 'ASC'}]
       columns[:key].label = "Key"
       columns[:value].label = "Value"
+      columns[:key].form_ui = :textarea
+      columns[:value].form_ui = :textarea
+      columns[:key].options = {:rows => 1, :columns => 40}
+      columns[:value].options = {:rows => 2, :columns => 60}
     end
   
   before_filter :check_admin_for_settings
