3604d7d75d13bcb1fab086891baa13f884ec6672
pkinney <pkinney@gmail.com>
2010-03-08 21:04:02 +0000

    Added a new_or_update_from_params method Added safe before_destroy method
    git-svn-id: http://clinicdb.rubyforge.org/svn@173 d096a246-4d2d-4550-b7bf-e545da479d79

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -13,6 +13,11 @@ class Visit < ActiveRecord::Base
   after_save       EncryptionWrapper.new( Visit.columns.select{ |c| c.text? && c.name!='version' }.collect{ |c| c.name })
   after_find       EncryptionWrapper.new( Visit.columns.select{ |c| c.text? && c.name!='version' }.collect{ |c| c.name })
   after_initialize EncryptionWrapper.new( Visit.columns.select{ |c| c.text? && c.name!='version' }.collect{ |c| c.name })
+  
+  before_destroy {|visit| 
+    visit.users.clear
+    VisitIdentifier.delete_for_visit(visit)
+  }
 	
 	def after_find
   end
@@ -28,5 +33,31 @@ class Visit < ActiveRecord::Base
   def self.current_version
     #"soap"
     "hpi_auto"
+  end
+  
+  def self.new_or_update_from_params(params)
+    visit = params[:visit_id] ? Visit.update(params[:visit_id][:visit_id], params[:visit]) : Visit.new(params[:visit])
+    
+    visit.visit_date = Date.today unless visit.visit_date
+    
+    if(visit.temperature)
+      if(visit.temperature < 50)
+        visit.temperature = visit.temperature * 1.8 + 32
+      end
+    end
+    visit.users = []
+    
+    if params[:users_id1] && params[:users_id1]!=""
+      visit.users << User.find(params[:users_id1])
+    end
+    if params[:users_id2] && params[:users_id2]!=""
+      visit.users << User.find(params[:users_id2])
+    end
+    
+    if visit.save
+      visit
+    else
+      nil
+    end
   end
 end
