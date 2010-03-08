78d0d6330ccae2bae5247099e9893b5d876987a0
pkinney <pkinney@gmail.com>
2010-03-08 15:14:15 +0000

    Some changes to the visit pdf and also ability to justify data_boxes
    git-svn-id: http://clinicdb.rubyforge.org/svn@171 d096a246-4d2d-4550-b7bf-e545da479d79

--- app/helpers/print/print_helper.rb
+++ app/helpers/print/print_helper.rb
@@ -42,22 +42,27 @@ module Print::PrintHelper
     end
     
     pdf.font_size = 12
-    pdf.bounding_box [0.in, 9.75.in], :width => 7.5.in, :height => 0.8.in do
+    pdf.bounding_box [0.in, 9.75.in], :width => 7.5.in, :height => 0.75.in do
       pdf.fill_color top_gray_level
       pdf.fill_and_stroke do
         pdf.rectangle pdf.bounds.top_left, pdf.bounds.width, pdf.bounds.height
         #pdf.stroke_bounds
       end
+      
       pdf.fill_color "000000"
       
-      pdf.bounding_box [0.1.in, 0.7.in], :width => 3.5.in, :height => 0.6.in do
-        pdf.text "#{visit.patient.last_name}, #{visit.patient.first_name} - ##{visit.patient.patient_number}", :leading => 2
-        pdf.text "DOB: #{visit.patient.dob_str}  (#{visit.patient.age})", :leading => 2
+      data_box pdf, "Patient #:", "#{visit.patient.patient_number}", 0.075.in, 1.75.in, pdf.bounds.height - 0.075.in, 0.25.in
+      data_box pdf, "Visit #:", "#{visit.visit_number}", 1.95.in, 1.75.in, pdf.bounds.height - 0.075.in, 0.25.in
+      
+      pdf.bounding_box [0.1.in, pdf.bounds.height-0.4.in], :width => 3.5.in, :height => 0.6.in do
+        #pdf.stroke_bounds
+        pdf.text "DOB: #{visit.patient.dob_str}    Age:#{visit.patient.age}", :leading => 2
         pdf.text "Race: #{visit.patient.ethnicity}    Sex: #{visit.patient.isMale? ? 'Male' : 'Female'}", :leading => 2
       end
       
-      pdf.bounding_box [3.in, 0.7.in], :width => 4.4.in, :height => 0.6.in do
-        pdf.text "#{visit.visit_date.strftime('%b %e, %G')} - Visit ##{visit.visit_number}", :align => :right, :leading => 2
+      pdf.bounding_box [3.in, 0.65.in], :width => 4.4.in, :height => 0.6.in do
+        pdf.text "#{visit.patient.last_name}, #{visit.patient.first_name}", :align => :right, :leading => 2
+        pdf.text "Visit on #{visit.visit_date.strftime('%b %e, %G')}", :align => :right, :leading => 2
         sess = Session.forDate(visit.visit_date)
         pdf.text "Attending: #{sess.attending.to_dr_label}", :align => :right, :leading => 2 if sess
         pdf.text "Seen by: #{visit.users.collect{|u| u.first_initial_last_name}.join(", ")}", :align => :right, :leading => 2
@@ -78,20 +83,25 @@ module Print::PrintHelper
   end
   
   def data_box(pdf, label, value, x_pos, width, y_pos=nil, height=nil, options ={})
+    align = options[:align] || :right
     pdf.bounding_box [x_pos, y_pos || 0.3.in], :width => width, :height => height || pdf.bounds.height do
       pdf.stroke_bounds
       
       pdf.font heading_font
       pdf.font_size options[:label_size] || options[:size] || 11
       h =  pdf.height_of label
+      w = pdf.width_of label
       b = (pdf.bounds.height - h)/2
       pdf.draw_text label, :at => [0.1.in, b+h/6]
       
       pdf.font data_font
       pdf.font_size options[:value_size] || options[:size] || 11
-      h =  pdf.height_of value
+      h =  (pdf.height_of value) + 0.01.in
       b = (pdf.bounds.height - h)/2
-      pdf.draw_text value, :at => [0.75.in, b+h/6]
+      #pdf.draw_text value, :at => [0.75.in, b+h/6]
+      pdf.bounding_box [w+0.2.in, h+b-h/6], :width => width-w-0.45.in, :height => h do
+        pdf.text value, :align => align, :valign => :center
+      end
     end
   end
   
