56af827962ebc2ec1cbbda2b1c0933f8efe29fde
Powell Kinney <pkinney@gmail.com>
2011-03-01 19:11:11 -0600

    First attempt at upgrading to Rails 3.  Just a fresh deploy of Rails copied over the corresponding files.  No changes to app/ or lib/

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClinicDB::Application.config.secret_token = 'c1f34432f6bfb43617b72f2d3d96ebbb82b752addb19783bcd1649829926ec8a9ffbaad6a62dd3d7143585095ae2a95ceebfd0c0fc9b9914d6b64d9092d4c3f9'
